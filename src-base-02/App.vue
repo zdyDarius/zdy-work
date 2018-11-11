24b7f21daf2de6208d91d54f2b0750e435d0cb4c
zdy <zdy080228@gmail.com>
2018-11-11 15:55:24 +0800

    完成了axios的小练习

---
+++ src-base-02/App.vue
@@ -0,0 +1,47 @@
+<template>
+<div>
+  <h2 v-if="!repname">Loding....</h2>
+  <h2 v-else>
+    most star repo is
+    <a :href="repurl">{{repname}}</a>
+  </h2>
+</div>
+</template>
+
+<script>
+import axios from 'axios'
+  export default {
+   async mounted(){
+
+     try {
+       const response= await axios.get('https://api.github.com/search/repositories88?q=vue&sort=stars')
+       this.repname=response.data.items[0].name // 得到返回结果数据
+       this.repurl=response.data.items[0].html_url
+     }catch (e){
+      console.log(555)
+       alert('网络状态不稳定，请稍后重试')
+     }
+
+    },
+    data(){
+      return {
+       repname:'',
+       repurl:''
+      }
+    },
+
+  }
+</script>
+
+
+<style scoped>
+  .todo-container {
+    width: 600px;
+    margin: 0 auto;
+  }
+  .todo-container .todo-wrap {
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+  }
+</style>
